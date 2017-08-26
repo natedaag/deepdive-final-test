b8a75d9d089f4771efd6b64786fa505b5b3cfd50
Nathan <natedaage@yahoo.com>
2017-08-26 14:49:38 -0600

    and finish this commit

--- src/edu/cnm/deepdive/finaltest/FinalTest.java
+++ src/edu/cnm/deepdive/finaltest/FinalTest.java
@@ -4,30 +4,14 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.io;
 
 public class FinalTest {
   
   private static final String TEST_BUNDLE = "resources/test-input.dat";
 
   public static void main(String[] args) {
-    ResourceBundle messageBundle = null;
-    try {
-      messageBundle = ResourceBundle.getBundle(TEST_BUNDLE);
-    } catch (MissingResourceException ex) {
-     ex.printStackTrace();
-    }
+
   }
-  
-  protected void setupPool() {
-    ResourceBundle bundle = ResourceBundle.getBundle(wordList);
-    ArrayList pool = new ArrayList<>();
-    Enumeration<String> keyEnum = bundle.getKeys();
-    while ( keyEnum.hasMoreElements()) {
-      String key = keyEnum.nextElement();
-      String word = bundle.getString(key);
-      pool.add(word);
-    }
   }
 
-}
+
