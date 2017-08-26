fc65abcbb4b410984f2caffb3efc125133331533
Nathan <natedaage@yahoo.com>
2017-08-26 16:33:35 -0600

    beginning to shuffle inputArray

--- src/edu/cnm/deepdive/finaltest/FinalTest.java
+++ src/edu/cnm/deepdive/finaltest/FinalTest.java
@@ -1,14 +1,19 @@
 package edu.cnm.deepdive.finaltest;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class FinalTest {
     
   public static void main(String[] args) {
   
     Reader reader = new Reader("resources/test-input.dat");
+    float[][] input = reader.input;
     System.out.println(Arrays.toString(reader.input[0]));
-
+    for (float[] inputArray : input) {
+      Collections.shuffle(Arrays.asList(inputArray));
+      
+    }
   }
 }
 
