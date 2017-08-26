c42af0f42098eaf250840c32a4a8a6ed4d93fb92
Nathan <natedaage@yahoo.com>
2017-08-26 15:20:59 -0600

    able to print array of arrays - imported from test-input.dat

--- src/edu/cnm/deepdive/finaltest/FinalTest.java
+++ src/edu/cnm/deepdive/finaltest/FinalTest.java
@@ -1,17 +1,17 @@
 package edu.cnm.deepdive.finaltest;
 
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.Arrays;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class FinalTest {
-  
-  private static final String TEST_BUNDLE = "resources/test-input.dat";
-
+    
   public static void main(String[] args) {
+  
+    Reader reader = new Reader("resources/test-input.dat");
+    System.out.println(Arrays.toString(reader.input[0]));
 
   }
-  }
+}
 
 
